9a516dc551b7fc70697ea486ba00428dddc8525d
Alex <a.zarr@icloud.com>
2021-02-04 12:23:37 -0800

    Add new helpers

--- TodoList/Shared/TodoListApp.swift
+++ TodoList/Shared/TodoListApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct TodoListApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
+    var coreDataHelper = CoreDataHelper.shared
+    
     var body: some Scene {
         WindowGroup {
             TaskListView()
+                .environment(\.managedObjectContext, coreDataHelper.persistentContainer.viewContext)
         }
     }
 }
